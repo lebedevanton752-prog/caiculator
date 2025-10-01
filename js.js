a8605189c991fa91b57de3ad378ca1e79b804b3b
sasha burger 37 <PERDEZH@gmail.com>
2025-10-01 09:21:43 +0800

    цифры в строке бундука

--- js.js
+++ js.js
@@ -1,11 +1,41 @@
 window.onload = function () {
-    let inputField= document.getElementById('inputField');
+    let inputField = document.getElementById('inputField');
     let butDigits = document.querySelectorAll('.digits-wrap button');
+    let butMathOperations = document.querySelectorAll('.math-wrap button');
+    let number1 = 0;
+    let number2 = 0;
+    let clean = 0;
 
     butDigits.forEach(function (el) {
-        el.addEventListener('click',function (e) {
+        el.addEventListener('click', function (e) {
             console.log(e.target.innerText)
+
+            if (clean === 1) {
+                inputField.value ='';
+                clean = 0
+        }
+            inputField.value = inputField.value + e.target.innerText;
         })
+
     })
+    butMathOperations.forEach(function (el) {
+            el.addEventListener('click', function (e) {
+
+
+
+                    number1 = inputField.value
+                    clean=1
+                    console.log(e.target.innerText)
+
+                })
+
+
+            })
+
+
 }
+
+
+
+
 
