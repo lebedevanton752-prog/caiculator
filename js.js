43f3ad3054898f188b251e3afb161f6df2ff2183
sasha burger 37 <PERDEZH@gmail.com>
2025-10-08 09:18:59 +0800

    тупой нолик

--- js.js
+++ js.js
@@ -4,24 +4,30 @@ window.onload = function () {
     let butDigits = document.querySelectorAll('.digits-wrap button');
     let butMathOperations = document.querySelectorAll('.math-wrap button');
     let number1 = 0;
-    let number2 = 0;
     let clean = 0;
     let mathOperation = ''
+    let lenInput =1
+
 
     butDigits.forEach(function (el) {
         el.addEventListener('click', function (e) {
-            console.log(e.target.innerText)
+            console.log(lenInput + 'тугрик' +e.target.innerText)
 
             if (clean === 1) {
                 inputField.value = '';
                 clean = 0
             }
+            if (lenInput === 1 && e.target.innerText ==='0'){
+                return
+            }
             inputField.value = inputField.value + e.target.innerText;
+            lenInput = lenInput + 1
         })
 
     })
     butMathOperations.forEach(function (el) {
         el.addEventListener('click', function (e) {
+            lenInput = 1
 
             if (e.target.innerText !== '=') {
                 number1 = inputField.value
@@ -38,21 +44,35 @@ window.onload = function () {
         console.log(number1)
         console.log(mathOperation)
         console.log(inputField.value)
-        if (mathOperation ==='+') {
-        inputField.value = +number1 + +inputField.value
+
+        let num2 = inputField.value
+
+        if (mathOperation === '+') {
+            inputField.value = +number1 + +inputField.value
         }
-        if (mathOperation ==='-') {
+        if (mathOperation === '-') {
             inputField.value = +number1 - +inputField.value
         }
-        if (mathOperation ==='*') {
+        if (mathOperation === '*') {
             inputField.value = +number1 * +inputField.value
         }
-        if (mathOperation ==='/') {
+        if (mathOperation === '/') {
             inputField.value = +number1 / +inputField.value
-        }
-    })
 
+            if (num2 === '0') {
+                inputField.value = 'ааа бугульдык ты очень тупое создание'
+                console.log('ыхыхы ты очень тупой бегаешь как говно')
+
+
+            }
+
+
+        }
+
+    })
 }
+
+
 // TODO
 // баг нолик
 // перв нол
